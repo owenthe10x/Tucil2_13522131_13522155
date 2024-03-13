de5f160f9a596c6ee2f667c1f2446bf03b7f508a
heisenberg <owentobias21@gmail.com>
2024-03-14 00:35:23 +0700

    chore: filename change

--- bruteforce.py
+++ bruteforce.py
@@ -1,12 +1,12 @@
 import pygame
 
 
-def linearBezier(positions, time, screen, color, draw=True):
-    x0 = (1 - time) * positions[0].x
-    y0 = (1 - time) * positions[0].y
+def linearBezier(positions, t, screen, color, draw=True):
+    x0 = (1 - t) * positions[0].x
+    y0 = (1 - t) * positions[0].y
 
-    x1 = time * positions[1].x
-    y1 = time * positions[1].y
+    x1 = t * positions[1].x
+    y1 = t * positions[1].y
 
     curve = (x0 + x1, y0 + y1)
 
@@ -31,16 +31,16 @@ def linearBezier(positions, time, screen, color, draw=True):
         return (int(curve[0]), int(curve[1]))
 
 
-def quadraticBezier(positions, time, screen, color, curve_list, green, draw=True):
+def quadraticBezier(positions, t, screen, color, curve_list, green, draw=True):
 
-    x0 = pow((1 - time), 2) * positions[0].x
-    y0 = pow((1 - time), 2) * positions[0].y
+    x0 = pow((1 - t), 2) * positions[0].x
+    y0 = pow((1 - t), 2) * positions[0].y
 
-    x1 = 2 * (1 - time) * time * positions[1].x
-    y1 = 2 * (1 - time) * time * positions[1].y
+    x1 = 2 * (1 - t) * t * positions[1].x
+    y1 = 2 * (1 - t) * t * positions[1].y
 
-    x2 = time**2 * positions[2].x
-    y2 = time**2 * positions[2].y
+    x2 = t**2 * positions[2].x
+    y2 = t**2 * positions[2].y
 
     curve = (x0 + x1 + x2, y0 + y1 + y2)
     if draw == True:
@@ -61,8 +61,8 @@ def quadraticBezier(positions, time, screen, color, curve_list, green, draw=True
         first_line = [positions[0], positions[1]]
         second_line = [positions[1], positions[2]]
 
-        a = linearBezier(first_line, time, screen, green, False)
-        b = linearBezier(second_line, time, screen, green, False)
+        a = linearBezier(first_line, t, screen, green, False)
+        b = linearBezier(second_line, t, screen, green, False)
 
         pygame.draw.line(screen, green, a, b, 2)
         pygame.draw.circle(screen, color, (int(curve[0]), int(curve[1])), 8)
