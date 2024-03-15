14d83d8ab7ff77a3ad0c777089e0e14b2c2a468c
Axel Santadi <axelsantadi@gmail.com>
2024-03-15 20:02:46 +0700

    update on bruteforce.py
    sekarang instead of pygame, outputnya ada di matplotlib

--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,9 @@
-import pygame
+import matplotlib.pyplot as plt
+import numpy as np
+#import pygame
 
 
-def linearBezier(positions, t, screen, color, draw=True):
+'''def linearBezier(positions, t, screen, color, draw=True):
     x0 = (1 - t) * positions[0].x
     y0 = (1 - t) * positions[0].y
 
@@ -29,41 +31,30 @@ def linearBezier(positions, t, screen, color, draw=True):
     elif draw == False:
         pygame.draw.circle(screen, color, (int(curve[0]), int(curve[1])), 8)
         return (int(curve[0]), int(curve[1]))
+'''
 
+def quadratic_bezier(positions, t):
+    x0 = pow((1 - t), 2) * positions[0][0]
+    y0 = pow((1 - t), 2) * positions[0][1]
 
-def quadraticBezier(positions, t, screen, color, curve_list, green, draw=True):
+    x1 = 2 * (1 - t) * t * positions[1][0]
+    y1 = 2 * (1 - t) * t * positions[1][1]
 
-    x0 = pow((1 - t), 2) * positions[0].x
-    y0 = pow((1 - t), 2) * positions[0].y
+    x2 = pow(t, 2) * positions[2][0]
+    y2 = pow(t, 2) * positions[2][1]
 
-    x1 = 2 * (1 - t) * t * positions[1].x
-    y1 = 2 * (1 - t) * t * positions[1].y
+    return [x0 + x1 + x2, y0 + y1 + y2]
 
-    x2 = t**2 * positions[2].x
-    y2 = t**2 * positions[2].y
+def plot_curve(positions):
+    t_values = np.linspace(0, 1, 100)
+    curve_points = np.array([quadratic_bezier(positions, t) for t in t_values])
 
-    curve = (x0 + x1 + x2, y0 + y1 + y2)
-    if draw == True:
-        pygame.draw.line(
-            screen,
-            (0, 0, 0),
-            (positions[0].x, positions[0].y),
-            (positions[1].x, positions[1].y),
-            1,
-        )
-        pygame.draw.line(
-            screen,
-            (0, 0, 0),
-            (positions[1].x, positions[1].y),
-            (positions[2].x, positions[2].y),
-            1,
-        )
-        first_line = [positions[0], positions[1]]
-        second_line = [positions[1], positions[2]]
+    plt.figure()
+    plt.plot(curve_points[:, 0], curve_points[:, 1], 'b-')
+    plt.plot([p[0] for p in positions], [p[1] for p in positions], 'ro-')
+    plt.show()
 
-        a = linearBezier(first_line, t, screen, green, False)
-        b = linearBezier(second_line, t, screen, green, False)
-
-        pygame.draw.line(screen, green, a, b, 2)
-        pygame.draw.circle(screen, color, (int(curve[0]), int(curve[1])), 8)
-    curve_list.append((int(curve[0]), int(curve[1])))
+# Example usage:
+if __name__ == "__main__":
+    positions = [[0, 0], [1, 3], [2, -1]]
+    plot_curve(positions)
